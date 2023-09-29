09de22090540febe5d7e182a0316c09113995e3a
dwunger <106967706+dwunger@users.noreply.github.com>
2023-09-29 01:05:06 -0400

    completed startswith implementation

--- heads-first-c/practice/str-mut.c
+++ heads-first-c/practice/str-mut.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int startswith_len(const char * str, const char * sub_str, int len);
+int startswith(const char * str, const char * sub_str);
 int length(const char * str);
+
 int main()
 {
     char my_str[] = "Doggo";
     int len = sizeof(my_str) / sizeof(my_str[0]);
-    printf("length(): %d\nSizeof(): %d", length(my_str), len);
-    if (startswith_len(my_str, "Dog", len))
-    {
-        printf("my_str starts with %s", "Dog");
+    printf("length(): %d\nSizeof(): %d\n", length(my_str), len);
+    if (startswith(my_str, "Dog")){
+        printf("%s starts with %s", my_str, "Dog");
     }
     return 0;
 }
 int length(const char * str)
 {
     int len = 0;
-
-    while (*str != '\0')
-    {
+    while (*str != '\0'){
         str++;
         len++;
     }
-    //Includes null terminating character in length
+    //+1 null terminator
     return len + 1;
 }
-int startswith_len(const char * str, const char * sub_str, int len)
+int startswith(const char * str, const char * sub_str)
 {
-    for (int i = 0; i < len; i++)
-    {
-        //todo: implement length()
-        continue;
+    int len = length(sub_str) - 1;
+    //Order of base conditions is important
+    //Base condition: an empty substring should always produce a match
+    if (len == 0){
+        return true;
     }
-    return 0;
+    //Base condition: checking a null string should not produce a match
+    if (str[0] == '\0'){
+        return false;
+    }
+    
+    for (int i = 0; i < len; i++){
+        // 
+        if (str[i] != sub_str[i]){
+            return false;
+        }
+    }
+    return true;       
+
 }
