b7e89d0975a67c7818c6047ad784e044def96a74
dwunger <106967706+dwunger@users.noreply.github.com>
2023-10-01 02:27:16 -0400

    porked lstrip function

--- heads-first-c/practice/str-mut.c
+++ heads-first-c/practice/str-mut.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
 int startswith(const char * str, const char * sub_str);
 int length(const char * str);
 
 int main()
 {
-    char my_str[] = "Doggo";
+    char my_str[] = "  Doggo";
     int len = sizeof(my_str) / sizeof(my_str[0]);
-    printf("length(): %d\nSizeof(): %d\n", length(my_str), len);
-    if (startswith(my_str, "Dog")){
-        printf("%s starts with %s", my_str, "Dog");
-    }
+    // printf("length(): %d\nSizeof(): %d\n", length(my_str), len);
+    // if (startswith(my_str, "Dog")){
+    //     printf("%s starts with %s", my_str, "Dog");
+    // }
+    my_str = lstrip(my_str);
+    puts(my_str);
     return 0;
 }
 int length(const char * str)
@@ -25,7 +27,7 @@ int length(const char * str)
     */
     //+1 null terminator
     //read the docs
-    int len = strlen(str)
+    int len = strlen(str);
     return len + 1;
 }
 int startswith(const char * str, const char * sub_str)
@@ -49,4 +51,27 @@ int startswith(const char * str, const char * sub_str)
     }
     return true;       
 
+}
+char * lstrip(char * str)
+{
+    int len = strlen(str) + 1;
+    int removed = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] != ' ')
+        {
+            //re-terminate the string
+            for (int j = len - removed; j < len; j++)
+            {
+                str[j] = '\0';
+            }
+            break;
+        }
+        if (str[i] == ' ')
+        {
+            removed++;
+            str[i] = str[i+1];
+        }
+    }
+    return str;
 }
