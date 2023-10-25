839a3f44eb4b653cebc13593adf40a2eb30f3b08
dwunger <106967706+dwunger@users.noreply.github.com>
2023-10-25 04:04:19 -0400

    very basic double ptr example

---
+++ practice/double_ptr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    // Single pointer
+    char array_of_char[10] = "Array of c";
+	printf(array_of_char);
+    // Double pointer
+	
+	char array_of_array[10][2] = {"A\0", "r\0", "r\0", "a\0", "y\0", " \0",
+			"o\0", "f\0", " \0", "A\0"};
+	for (int i = 0; i < 10; i++) {
+		printf(array_of_array[i]);
+	}
+
+	return 0;
+
+}
+
