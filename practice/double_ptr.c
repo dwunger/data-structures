257b1b63b52f597bdbe6a9161fb104d66b8d604a
dwunger <106967706+dwunger@users.noreply.github.com>
2023-10-30 04:54:52 -0400

    distinction between ref swap and value swap

--- practice/double_ptr.c
+++ practice/double_ptr.c
@@ -3,6 +3,7 @@
 
 void ptr_swap(int ** number1, int ** number2)
 {
+    // swap the pointer address reference
     if (!number1 || !number2) {
         printf("Received Null pointer");
         return;
@@ -13,8 +14,19 @@ void ptr_swap(int ** number1, int ** number2)
     *number2 = temp_ptr;
 }
 
+void int_swap(int * number1, int * number2)
+{
+    if (!number1 || !number2) {
+        fprintf(stderr, "Dereferenced null pointer\n");
+    }
+    int temp = *number1;
+    *number1 = *number2;
+    *number2 = temp;
+}
+
 int main(void) 
 {
+    puts("======INTEGER======");
     int num1 = 1;
     int num2 = 2;
     
@@ -24,8 +36,16 @@ int main(void)
     printf("Number 1: %d\nNumber 2: %d\n", *ptr_num1, *ptr_num2);
 
     ptr_swap(&ptr_num1, &ptr_num2);
+    puts("ptr_swap(&ptr_num1, &ptr_num2);");
 
     printf("Number 1: %d\nNumber 2: %d\n", *ptr_num1, *ptr_num2);
+    
+    int_swap(&num1, &num2); 
+    puts("int_swap(&num1, &num2);");
+
+    printf("Number 1: %d\nNumber 2: %d\n", *ptr_num1, *ptr_num2);
+    
+    puts("======STRINGS======");
 
     return 0;
 }
