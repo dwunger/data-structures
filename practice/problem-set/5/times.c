3e0f04864f62c026574c677088adcfedbedf4163
Denton Unger <106967706+dwunger@users.noreply.github.com>
2023-11-03 03:21:17 -0400

    solution to generate times table

--- practice/problem-set/5/times.c
+++ practice/problem-set/5/times.c
@@ -1,7 +1,7 @@
 /*
 
 5) Write a program to print the multiplication table in the following format.
-1 2 3 4 5
+  1 2 3 4 5
 1 1 2 3 4 5
 2 2 4 6 8 10
 3 3 6 9 12 15
@@ -10,21 +10,37 @@
 
 */
 
-include <stdio.h>
-#define RANGE 5
+#include <stdio.h>
+#define RANGE 10
 
-int main(void) 
+int main(void)
 {
-    int x = 1;
-    int y = 1;
-    puts("  1 2 3 4 5");
 
-    for (int i = 0; i < RANGE; i++)
+    printf("%2s", "");
+    // CREATE TABLE HEADER
+    //   1 2 3 4 5 ... RANGE
+    for (int i = 1; i < RANGE; i++)
     {
-        printf("%d", (i + 1));
-i       printf(" %d", (x +
+        printf("%5d", (i));
+    }
+    
+    for (int y = 1; y < RANGE; y++)
+    {
+        // ADD LEADING DIGIT WITHOUT TRAILING SPACE 
+        // 1
+        // ...
+        // RANGE
+        printf("\n%2d", y);
+        
+        for (int x = 1; x < RANGE; x++)
+        {
+            // APPEND LEFT PADDED DIGIT TO ROW
+            printf("%5d", (x * y));
+        }
+        
+         
     }
 
+    return 0;
+
 }
-
-
