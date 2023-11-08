ccff2c9c088a4fca1a855453debdb1b42fbfb9d1
Denton Unger <106967706+dwunger@users.noreply.github.com>
2023-11-08 03:39:20 -0500

    Identify palindromes

--- practice/problem-set/palindrome.c
+++ practice/problem-set/palindrome.c
@@ -5,7 +5,6 @@
 #include <string.h>
 
 int is_palindrome(const char *str);
-int is_palindrome_slow(const char *str);
 
 int main(void)
 {
@@ -20,7 +19,7 @@ int main(void)
     test = is_palindrome(false_palindrome);
     
     assert(test == 0);
-    puts("All tests passing!")
+    puts("All tests passing!");
     return 0;
 }
 
@@ -30,11 +29,15 @@ int is_palindrome(const char *str)
         fprintf(stderr, "Received unitialized pointer");
     }
     int len = strlen(str);
-    for (int i = 0; i < len; i++) {
+    // a palindrome must have a non-zero count of characters
+    if (len == 0) {
+        return 0;
+    }
+    // checking full length of string is redundant
+    for (int i = 0; i < len/2; i++) {
         //printf("%c != %c\n", str[i], str[len -1 - i]);
         if (str[i] != str[len - 1 - i]) return 0;
     }
     return 1;
     
 }
-int is_palindrome_slow(const char *str);
