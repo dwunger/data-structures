1f834c7e0827dcca12b5f6bf0d9a9860acfc8644
dwunger <106967706+dwunger@users.noreply.github.com>
2023-11-11 17:21:24 -0500

    Perfect numbers

--- practice/problem-set/12/perfect.c
+++ practice/problem-set/12/perfect.c
@@ -4,7 +4,7 @@
 // 1 + 2 + 3 = 6 
 
 #include <stdio.h>
-
+#include <stdlib.h>
 int get_proper_divisors(const int value, int *divisors, int *len) {
     if (!divisors || !len) {
         return -1;
@@ -17,17 +17,42 @@ int get_proper_divisors(const int value, int *divisors, int *len) {
         }
     } 
 }
-int main(void) {
-    int test_value = 6;
-    int proper_divisors[256];
-    int len = 0;
 
+int is_perfect(const int integer) {
+    int *divisors = malloc(1024);
+    int len = 0;
+    
+    get_proper_divisors(integer, divisors, &len);
+    
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum += divisors[i];
+    }
+
+    free(divisors);
+    return (sum == integer);
+}
+int main(void) {
+    int test_value = 2147483646;
+    int *proper_divisors = malloc(1024);
+    int len = 0;
+    /*
     get_proper_divisors(test_value, proper_divisors, &len);
     
     for (int i = 0; i < len; i++) {
         printf("%d, ", proper_divisors[i]);
     }
+    */
 
+    int result = 0;
+    for (int i = 0; i < 100; i++) {
+        result = is_perfect(i);
+        printf("%3d : %d\n", i, result); 
+    }
+
+
+
+    free(proper_divisors);    
     return 0;
 }
 
