fab2e33e87e7eadd15688c16fd29fabdea36e6be
dwunger <106967706+dwunger@users.noreply.github.com>
2023-11-10 20:19:00 -0500

    Identify Armstrong ranges

--- practice/problem-set/11/armstrong.c
+++ practice/problem-set/11/armstrong.c
@@ -1,4 +1,7 @@
-//11) Write a program to check whether the given number is an Amstrong number or not.
+//11) Write a program to check whether the given number is an Amstrong number or not
+//
+// 371 => 3^3 + 7^3 + 1^3 = 371
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,15 +10,41 @@
 
 int is_armstrong(int number);
 
+void print_hyphenated_ranges(int arr[], int len) {
+    // start/end of current range
+    int start = arr[0]; 
+    int end; 
+
+    for (int i = 1; i < len; i++) {
+        if (arr[i] == arr[i - 1] + 1) {
+            end = arr[i];
+        } else {
+            if (start == arr[i - 1]) {
+                printf("%d, ", start);
+            } else {
+                printf("%d-%d, ", start, arr[i - 1]);
+            }
+            start = arr[i];
+        }
+    }
+
+    if (start == arr[len - 1]) {
+        printf("%d\n", start);
+    } else {
+        printf("%d-%d\n", start, arr[len - 1]);
+    }
+}
 
 int main(void) 
 {
     // Created temp indices to avoid making an append function or linked list
     int armstrongs[1000];
     int a_idx = 0;
+    int armstrongs_len = 0;
 
-    int non_armstrongs[1000];
+    int non_armstrongs[1000] = {0};
     int n_idx = 0;
+    int non_armstrongs_len = 0;
 
     int result = 0;
     int number = 371;
@@ -29,21 +58,16 @@ int main(void)
             n_idx++;
         }
 
-        //printf("%d : %d\n", i, result);
-    }
-    printf("Non-Armstrongs\n");
-    int last = -1;
-    printf("%d ", non_armstrongs[0]);
-    for (int i = 1; i < 1000; i++) {
-        last = non_armstrongs[i - 1];
-        if (last + 1 != non_armstrongs[i]) {
-            printf("- %d, ", non_armstrongs[i]);
-        } else {
-            //printf("", non_armstrongs[i]);
-            continue; 
-        }
+        printf("%d : %d\n", i, result);
     }
 
+    armstrongs_len = a_idx;
+    non_armstrongs_len = n_idx;
+
+    printf("Non-Armstrongs\n");
+    print_hyphenated_ranges(non_armstrongs, non_armstrongs_len);    
+    printf("Armstrongs\n");
+    print_hyphenated_ranges(armstrongs, armstrongs_len);
 
     return 0;
 }
