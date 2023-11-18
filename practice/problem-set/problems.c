5250dd43a26c5c2e4874a86fd4193c62bbbdd8fd
dwunger <106967706+dwunger@users.noreply.github.com>
2023-11-17 19:05:06 -0500

    Colorful problems

---
+++ practice/problem-set/problems.c
@@ -0,0 +1,151 @@
+
+
+#include <stdio.h>
+
+int main() {
+    printf("\033[0;36mUNIT-I\033[0m\n");
+
+    printf("1. Write a program to illustrate \033[1;31mbitwise operators\033[0m.\n");
+    printf("2. Write a program to print all the \033[1;31malphabets\033[0m and their equivalent \033[1;31mASCII values\033[0m.\n");
+    printf("3. Write a program to calculate the \033[1;31msum of squares\033[0m of first \033[1;31mn natural numbers\033[0m using while loop.\n");
+    printf("4. Write a program to illustrate \033[1;31mshorthand operators\033[0m used in C.\n");
+    printf("5. Write a program to print the \033[1;31mmultiplication table\033[0m in the following format:\n");
+    printf("\033[0;32m 1  2  3  4  5\033[0m\n");
+    printf("\033[0;32m 1  1  2  3  4  5\033[0m\n");
+    printf("\033[0;32m 2  2  4  6  8 10\033[0m\n");
+    printf("\033[0;32m 3  3  6  9 12 15\033[0m\n");
+    printf("\033[0;32m 4  4  8 12 16 20\033[0m\n");
+    printf("\033[0;32m 5  5 10 15 20 25\033[0m\n\n");
+    printf("6. Write a program to calculate the \033[1;31mfactorial\033[0m of a given number.\n");
+    printf("7. Write a program to calculate the \033[1;31msum of squares of cubes\033[0m of the first \033[1;31mn natural numbers\033[0m.\n");
+    printf("8. Write a program to \033[1;31mreverse\033[0m the given number.\n");
+    printf("9. Write a program to calculate \033[1;31mmn value\033[0m using do-while loop.\n");
+    printf("10. Write a program to check whether the given number is a \033[1;31mpalindrome\033[0m or not.\n");
+    printf("11. Write a program to check whether the given number is an \033[1;31mArmstrong number\033[0m or not.\n");
+    printf("12. Write a program to check whether the given number is a \033[1;31mperfect number\033[0m or not.\n");
+    printf("13. Write a program to print the following format:\n");
+    printf("\033[0;32m*\n* *\n* * *\n* * * *\n* * * * *\n\033[0m\n");
+    printf("14. Program to calculate \033[1;31mlucky number\033[0m.\n");
+    printf("15. Write a program to calculate the result of the series accurate up to the 7th digit:\n");
+    printf("\033[1;31mx + x^3/3! + x^5/5! + ...\033[0m\n");
+    printf("16. An electric power distribution company charges its domestic consumers as follows:\n");
+    printf("- Consumption Units    Rate of Charge\n");
+    printf("- \033[1;31m0-200\033[0m               Rs. 0.50 per unit\n");
+    printf("- \033[1;31m201-400\033[0m             Rs. 100 plus Rs. 0.65 per unit excess of 200\n");
+    printf("- \033[1;31m401-600\033[0m             Rs. 230 plus Rs. 0.80 per unit excess of 400.\n");
+    printf("Write a C program that reads the customer number and power consumed and prints the amount to be paid by the customer.\n");
+
+    printf("\n\033[0;36mUNIT-II\033[0m\n");
+
+    printf("17. Write a program to print the elements of an \033[1;31marray\033[0m in \033[1;31mreverse order\033[0m.\n");
+    printf("18. Write a program to \033[1;31madd all the elements\033[0m of a \033[1;31mtwo-dimensional array\033[0m.\n");
+    printf("19. Write a program to find the \033[1;31mtranspose\033[0m of a given \033[1;31mmatrix\033[0m.\n");
+    printf("20. Write a program to find the \033[1;31msmallest\033[0m and \033[1;31mlargest element\033[0m in a \033[1;31mtwo-dimensional array\033[0m.\n");
+    printf("21. Write a program to illustrate \033[1;31mcall by value\033[0m.\n");
+    printf("22. Write a function to calculate the \033[1;31mfactorial\033[0m of a given \033[1;31mnumber\033[0m.\n");
+    printf("23. Write a function to \033[1;31msort the elements\033[0m of an \033[1;31marray\033[0m.\n");
+    printf("24. Write a \033[1;31mrecursive function\033[0m to find the \033[1;31mroots\033[0m of the \033[1;31mquadratic expression\033[0m using the \033[1;31mbisection method\033[0m.\n");
+    printf("25. Write a program to \033[1;31mcopy a string\033[0m to another without using \033[1;31mstring handling functions\033[0m.\n");
+    printf("26. Write a program to check whether a given \033[1;31mstring\033[0m is a \033[1;31mpalindrome\033[0m or not.\n");
+    printf("27. Write a function to find the \033[1;31mlargest element\033[0m in an \033[1;31marray\033[0m.\n");
+    printf("28. Write a \033[1;31mrecursive function\033[0m `power(base, exponent)` that returns \033[1;31mbase^exponent\033[0m.\n");
+    printf("29. Program to \033[1;31msort the characters\033[0m in a given \033[1;31mstring\033[0m.\n");
+    printf("30. Write a function to convert all the \033[1;31muppercase letters\033[0m to \033[1;31mlowercase\033[0m and \033[1;31mlowercase letters\033[0m to \033[1;31muppercase\033[0m in a given \033[1;31mstring\033[0m.\n");
+    printf("31. Program to \033[1;31mundefine an already defined macro\033[0m.\n");
+    printf("32. Program to illustrate \033[1;31mconditional compilation\033[0m using \033[1;31m`#if`, `#endif`, `#else`\033[0m.\n");
+
+    printf("\n\033[0;36mUNIT-III\033[0m\n");
+
+    printf("33. Write a program to calculate the length of a \033[1;31mstring\033[0m using \033[1;31mpointers\033[0m.\n");
+    printf("34. Write a function to \033[1;31mswap two variables\033[0m using \033[1;31mpointers\033[0m.\n");
+    printf("35. Program to illustrate \033[1;31mpointer arithmetic\033[0m.\n");
+    printf("36. Write a function to calculate the \033[1;31msum of two numbers\033[0m using \033[1;31mpointers to functions\033[0m.\n");
+    printf("37. Write a program to perform \033[1;31mmatrix multiplication\033[0m using \033[1;31mpointers\033[0m.\n");
+    printf("38. Write a program to find the \033[1;31mlargest element\033[0m in an \033[1;31marray\033[0m using \033[1;31mpointers\033[0m.\n");
+    printf("39. Program to arrange the given numbers in \033[1;31mascending order\033[0m.\n");
+    printf("40. Write a C program using a \033[1;31mpointer\033[0m for \033[1;31mstring comparison\033[0m.\n");
+    printf("41. Program to \033[1;31mreverse the string\033[0m using \033[1;31mpointers\033[0m.\n");
+    printf("42. Program to perform \033[1;31msorting\033[0m using \033[1;31mcommand-line arguments\033[0m.\n");
+    printf("43. The names of employees of an organization are stored in three arrays, namely, \033[1;31mfirst name\033[0m, \033[1;31msecond name\033[0m, and \033[1;31mlast name\033[0m. Write a program using \033[1;31mpointers\033[0m to \033[1;31mconcatenate the three parts\033[0m into one \033[1;31mstring\033[0m to be called the \033[1;31mname\033[0m.\n");
+    printf("44. Program to \033[1;31mmerge two sorted arrays\033[0m so that the resultant \033[1;31marray is in sorted order\033[0m using \033[1;31mpointers\033[0m.\n");
+
+    printf("\n\033[0;36mUNIT-IV\033[0m\n");
+
+    printf("45. Write a C program to compute the monthly pay of 100 employees using each employee's name and \033[1;31mbasic pay\033[0m. The \033[1;31mDA\033[0m is computed as \033[1;31m52% of the basic pay\033[0m. Gross salary (\033[1;31mBasic pay + DA\033[0m). Print the employees' \033[1;31mnames\033[0m and \033[1;31mgross salary\033[0m.\n");
+    printf("46. Write a program to calculate and print \033[1;31mstudent-wise total\033[0m for \033[1;31m50 students\033[0m and \033[1;31m3 subjects\033[0m. The structure should contain \033[1;31m3 subjects and total\033[0m.\n");
+    printf("47. Write a program to calculate and print \033[1;31mstudent-wise total\033[0m for \033[1;31m50 students\033[0m and \033[1;31m3 subjects\033[0m using \033[1;31mpointers\033[0m. The structure should contain \033[1;31m3 subjects\033[0m.\n");
+    printf("48. Write a program to store the information of \033[1;31mvehicles\033[0m using \033[1;31mbit fields\033[0m to store the \033[1;31mstatus information\033[0m. Assume the vehicle object consists of \033[1;31mtype, fuel, and model\033[0m member fields. Assume an appropriate number of \033[1;31mbits for each field\033[0m.\n");
+    printf("49. Program for the illustration of \033[1;31muser-defined data types\033[0m using \033[1;31mtypedef\033[0m.\n");
+    printf("50. Program for the illustration of \033[1;31mnested structures\033[0m.\n");
+    printf("51. Program to \033[1;31madd or delete a record\033[0m of a particular \033[1;31memployee\033[0m based on his \033[1;31mcode\033[0m. The structure should contain \033[1;31mname, designation, code, and salary\033[0m. The program should also provide the flexibility to \033[1;31mupdate any employee's record\033[0m.\n");
+    printf("52. Define a structure that represents a \033[1;31mcomplex number\033[0m (contains two floating-point members, called \033[1;31mreal and imaginary\033[0m). Write functions to \033[1;31madd, subtract, multiply, and divide two complex numbers\033[0m.\n");
+    printf("53. A company markets \033[1;31mhardware items\033[0m. Create a structure \033[1;31m\"hwitem\"\033[0m that stores the \033[1;31mtitle\033[0m of the item, its \033[1;31mprice\033[0m, an array of \033[1;31mthree floats\033[0m so that it can record the sale in rupees of a particular item for the \033[1;31mlast three months\033[0m, \033[1;31mcategory\033[0m of the item, and its \033[1;31moriginal equipment manufacturer\033[0m. Write a short program that provides the facility to \033[1;31mread N numbers of items' information, append a new item, and display all records\033[0m.\n");
+    printf("54. Define a structure to represent a \033;31mdate\033[0m. Use your structures to \033[1;31maccept two different dates\033[0m in the format \033[1;31mmmdd\033[0m of the same year. Write a C program to display the \033[1;31mmonth names\033[0m of both dates.\n");
+    printf("55. Consider a structure \033[1;31mmaster\033[0m that includes information like \033[1;31mname, code, pay, and experience\033[0m. Write a program to \033[1;31mdelete and display the information\033[0m contained in \033[1;31mmaster variables\033[0m for a given \033[1;31mcode\033[0m.\n");
+    printf("56. Write a program to use a \033[1;31mstructure within a union\033[0m. Display the contents of \033[1;31mstructure elements\033[0m.\n");
+    
+    printf("\n\033[0;36mUNIT-V\033[0m\n");
+
+    printf("57. Write a C program to read a \033[1;31mtext file\033[0m and count the number of \033[1;31mcharacters, number of words, and the number of sentences\033[0m. Write the results in an \033[1;31moutput file\033[0m.\n");
+    printf("58. Write a C program to read a \033[1;31mtext file\033[0m containing some \033[1;31mparagraphs\033[0m. Use \033[1;31mfseek()\033[0m and read the text after \033[1;31mskipping n characters\033[0m from the \033[1;31mbeginning of the file\033[0m.\n");
+    printf("59. Write a C program to read a \033[1;31mtext file\033[0m containing some \033[1;31mparagraphs\033[0m. Print the \033[1;31mfirst n characters\033[0m from the \033[1;31mbeginning of the file\033[0m.\n");
+    printf("60. Write a program to print the output in the following format in an \033[1;31mOUTPUT file\033[0m:\n");
+    printf("```\n Number  Square  Cube\n 2       4       8\n 3       9       27\n ```\n");
+    printf("61. Write a C program to read data from a file named \033[1;31mINPUT\033[0m containing \033[1;31mnumbers\033[0m. Write all \033[1;31meven numbers\033[0m to a file called \033[1;31mEVEN\033[0m and \033[1;31modd numbers\033[0m to a file called \033[1;31mODD\033[0m.\n");
+    printf("62. Program to print the \033[1;31mn characters\033[0m from the \033[1;31mmth position\033[0m in the \033[1;31mfile\033[0m.\n");
+    printf("63. Program to print the \033[1;31mcurrent position value\033[0m of the \033[1;31mfile pointer\033[0m.\n");
+    printf("64. Program to check whether the \033[1;31mend of the file\033[0m has been reached or not using the \033[1;31mfeof()\033[0m function.\n");
+    printf("65. Program to check whether any \033[1;31mdata errors\033[0m are present and print the relevant information using the \033[1;31mferror()\033[0m function.\n");
+    printf("66. Write a program to \033[1;31mdetect an error\033[0m while opening a \033[1;31mfile\033[0m that does not exist.\n");
+    printf("67. Write a C program to open a \033[1;31mpre-existing file\033[0m and \033[1;31madd information\033[0m at the end of the file. Display the \033[1;31mcontents of the file\033[0m before and after \033[1;31mappending\033[0m.\n");
+    printf("68. Program to \033[1;31mcopy one file to another\033[0m.\n");
+    printf("69. Write a program to read a \033[1;31mtext file\033[0m and \033[1;31mconvert the file contents to uppercase\033[0m and write the contents to an \033[1;31moutput file\033[0m.\n");
+    printf("70. Candidates have to score \033[1;31m90 or above\033[0m in the \033[1;31mIQ test\033[0m to be considered \033[1;31meligible\033[0m for taking further tests. All candidates who do not clear the IQ test are sent \033[1;31mreject letters\033[0m, and others are sent \033[1;31mcall letters\033[0m for further tests. The selected list and other conversation have to be written to a \033[1;31mfile\033[0m.\n");
+
+    printf("\033[0m\n"); // Reset text color
+
+    printf("\n\033[0;36mUNIT-VI\033[0m\n");
+
+    printf("71. Write a non-recursive simulation of the \033[1;31mTowers of Hanoi\033[0m.\n");
+    printf("72. Write a \033[1;31mrecursive function\033[0m for the \033[1;31mTowers of Hanoi\033[0m.\n");
+    printf("73. Program to convert \033[1;31mPrefix to postfix\033[0m using \033[1;31mstacks\033[0m.\n");
+    printf("74. Program to convert \033[1;31mpostfix expression to prefix expression\033[0m.\n");
+    printf("75. Write a program to \033[1;31mevaluate a postfix expression\033[0m.\n");
+    printf("76. Program to \033[1;31mevaluate a prefix expression\033[0m.\n");
+    printf("77. Write a program to implement \033[1;31mdequeues\033[0m.\n");
+    printf("78. Program to implement \033[1;31mpriority queues\033[0m.\n");
+    printf("79. Program to check whether a given \033[1;31mstring is palindrome\033[0m or not using \033[1;31mstacks\033[0m.\n");
+
+    printf("\033[0;36mUNIT-VII\033[0m\n");
+
+    printf("80. Write a program in 'C' to form a list consisting of the \033[1;31mintersection\033[0m of the elements of two lists.\n");
+    printf("81. Write a routine to \033[1;31mreverse elements\033[0m of a \033[1;31mdoubly linked list\033[0m by \033[1;31mtraversing the list only once\033[0m.\n");
+    printf("82. Write a program to count the number of \033[1;31mnon-leaf nodes\033[0m in a \033[1;31mbinary tree\033[0m.\n");
+    printf("83. Write a program to count the number of \033[1;31mleaf nodes\033[0m in a \033[1;31mbinary tree\033[0m.\n");
+    printf("84. Write a program to implement a \033[1;31mcircular linked list\033[0m using a \033[1;31mdouble linked list\033[0m.\n");
+    printf("85. Write a program to perform \033[1;31mpolynomial addition\033[0m.\n");
+    printf("86. Write a program to perform \033[1;31mpolynomial multiplication\033[0m.\n");
+    printf("87. Function to \033[1;31mreverse the singly linked list\033[0m.\n");
+    printf("88. Write \033[1;31mnon-recursive functions\033[0m for \033[1;31mbinary tree traversals\033[0m.\n");
+    printf("89. Program to implement a \033[1;31mbinary search tree\033[0m using \033[1;31marrays\033[0m.\n");
+    printf("90. Write a C program to \033[1;31mexchange two nodes\033[0m of a \033[1;31msingly linked list\033[0m.\n");
+    printf("91. Program to implement \033[1;31mbreadth-first search\033[0m.\n");
+    printf("92. Program to implement \033[1;31mdepth-first search\033[0m.\n");
+    printf("93. Represent a \033[1;31msingly linked list\033[0m using an \033[1;31marray\033[0m. Write routines to \033[1;31minsert and delete elements\033[0m for this representation.\n");
+    printf("94. Write a routine \033[1;31mSPLIT()\033[0m to \033[1;31msplit a singly linked list\033[0m into two lists so that all elements in \033[1;31modd positions\033[0m are in one list and those in \033[1;31meven positions\033[0m are in another list.\n");
+    printf("95. Represent a \033[1;31mdoubly linked list\033[0m using an \033[1;31marray\033[0m. Write routines to \033[1;31minsert and delete elements\033[0m for this representation.\n");
+    printf("96. Write a function to \033[1;31mmerge two doubly linked lists\033[0m and arrange the numbers in \033[1;31mascending order\033[0m.\n");
+
+    printf("\n\033[0;36mUNIT-VIII\033[0m\n");
+
+    printf("97. Write a program to implement \033[1;31mmerge sort\033[0m.\n");
+    printf("98. Derive the \033[1;31mefficiency of merge sort\033[0m.\n");
+    printf("99. Derive the \033[1;31mefficiency of binary search\033[0m.\n");
+
+    printf("\033[0m\n"); // Reset text color
+
+    return 0;
+
+
+}
+
