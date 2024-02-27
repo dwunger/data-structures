95e8c9afca48fecb463bb4514dd8b3192dc94c69
dwunger <106967706+dwunger@users.noreply.github.com>
2024-02-26 20:53:43 -0500

    Hello Messagebox

---
+++ windows/MessageBox/msgbox.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+/*
+`hInstance` is the handle to an instance or handle to a module. 
+            The operating system uses this value to identify the 
+            executable or EXE when it's loaded in memory. 
+            Certain Windows functions need the instance handle, 
+            for example to load icons or bitmaps.
+
+`hPrevInstance` has no meaning. It was used in 16-bit Windows,
+            but is now always zero.
+
+`pCmdLine` contains the command-line arguments as a Unicode string.
+
+`nCmdShow` is a flag that indicates whether the main application 
+            window is minimized, maximized, or shown normally.
+*/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    MessageBox(NULL, TEXT("Hello from USER32!"), TEXT("HelloMSG"), 0);
+     
+    return 0;
+}
