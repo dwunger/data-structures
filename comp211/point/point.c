1defa7cead5d9c2e15fb62ee7cd8fe2ad948f2da
Denton Unger <106967706+dwunger@users.noreply.github.com>
2024-04-06 06:38:53 -0400

    point module

--- comp211/point/point.c
+++ comp211/point/point.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Guards.h"
 
 Point* point_create(double x, double y) {
@@ -10,9 +12,31 @@ Point* point_create(double x, double y) {
     return new_point;
 }
 
-void point_destroy(Point *self);
+void point_destroy(Point *self) {
+    if (self != NULL) {
+        free(self);
+    } else {
+        fprintf(stderr, "Cannot dellocate memory at address %p\n", self);
+    }
+}
 
-void point_print(Point *self);
+void point_print(Point *self) {
+    if (self != NULL) {
+        printf("Point = {%f, %f}", (*self).x, (*self).y);
+    } else {
+        fprintf(stderr, "Cannot print point at address: %p\n", self);
+    }
+}
 
-Point* point_offset(Point self, double x, double y);
+Point* point_offset(Point *self, double x, double y) {
+    if (self == NULL) {
+        fprintf(stderr, "point_offset received pointer to null\n");
+        return;
+    }  
 
+    //(*self).x += x;
+    self -> x += x;
+
+    //(*self).y += y;
+    self -> y += y;    
+}
